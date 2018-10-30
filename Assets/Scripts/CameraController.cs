751e81489c95b9a06b09276298e85ebb889f3311
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2018-10-29 23:26:18 -0400

    UnityFilesUpdate_v1.1
    Player Controls
    Game Controls WIP
    Camera follows player (respectively)
    Simple UI

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+
+    private GameObject cameraTarget;
+    private Vector3 offset;
+
+    void Start () {
+        if (this.CompareTag("Camera1")){
+            cameraTarget = player1;
+        }
+        if (this.CompareTag("Camera2")){
+            cameraTarget = player2;
+        }
+        offset = transform.position - cameraTarget.transform.position;
+    }
+	
+	
+	void Update () {
+        transform.position = cameraTarget.transform.position + offset;
+    }
+    void LateUpdate()
+    {
+        offset = transform.position - cameraTarget.transform.position;
+        transform.LookAt(cameraTarget.transform.position);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject[] levels;
+    public Vector3 levelSpawnValues;
+    public int levelCount;
+    public float levelSpawnWait;
+    public float startWait;
+    public float waveWait;
+    public int numRounds = 1;
+    public Text scoreText1;
+    public Text scoreText2;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private bool roundOver;
+    private int score1;
+    private int score2;
+
+    private void Start()
+    {
+        gameOver = false;
+        restart = false;
+        //restartText.text = "";
+        //gameOverText.text = "";
+        score1 = 0;
+        score2 = 0; 
+        UpdateScore();
+        StartCoroutine(SpawnLevels());
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                //Application.LoadLevel (Application.loadedLevel);
+                SceneManager.LoadScene("TumbleTemple", LoadSceneMode.Single);
+            }
+        }
+    }
+
+    IEnumerator SpawnLevels()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int rounds = 0; rounds < numRounds; rounds++)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    break;
+                }
+            }
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                break;
+            }
+        }
+    }
+
+    public void AddScore(string playerName, int newScoreValue)
+    {
+        if (playerName == "Player1")
+        {
+            score1 += newScoreValue;
+        }
+        if (playerName == "Player2")
+        {
+            score2 += newScoreValue;
+        }
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        //scoreText1.text = " Player1 Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+    public void RoundOver()
+    {
+        roundOver = true;
+    }
+}
